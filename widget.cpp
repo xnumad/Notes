8bc84dc69862834b854dc902c1ef1384bb160996
xnumad <34810600+xnumad@users.noreply.github.com>
2020-01-08 17:07:26 +0100

    Rebuild index before saving
    Fixes bug in deleted code comment and "Opening application without file and saving more than once leads to crash because startLines vector is not being updated"

--- widget.cpp
+++ widget.cpp
@@ -154,6 +154,8 @@ void Widget::on_btnPreviousNote_clicked() //event to load the previous note, tri
 void Widget::on_btnSave_clicked() //Saving procedure of the file, saveNotes()
 {
     QVector<string> notes; //QVector to cache the notes in the file before it will be truncated
+    loadNotes(); //Rebuild index to prevent the save procedure from reading an empty file (because file.good() returns true and setupFile() will also succeed) that was only created during the attempt to read a non-existant notes file.
+
     if (setupFile()) //file is good
     {
         //Fill the notes vector
@@ -181,7 +183,6 @@ void Widget::on_btnSave_clicked() //Saving procedure of the file, saveNotes()
     //The part where to modify the data.
     //Using the from the file fetched data, it is now being "merged" with the editor data from the UI by overwriting that note
     qDebug() << "Saved: " << ui->txtNote->toPlainText();
-    //This line seems to be the culprit for a bug in which if you have two notes of which both only have one line and add a few lines to the first one, the second one loses its content
     notes[ui->lblNoteValue->text().toInt() - 1] = ui->txtNote->toPlainText().toStdString();
 
     setupFile(true); //open file again, but this time to write (finally actually save) to it
